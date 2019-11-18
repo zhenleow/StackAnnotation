61a76612594816efb3700b0aa84d70b92f7d38bd
zhenleow <leowzhen@gmail.com>
2019-11-18 16:52:05 +0800

    Update MyStringStack.java

--- src/stack/MyStringStack.java
+++ src/stack/MyStringStack.java
@@ -33,6 +33,7 @@ public class MyStringStack {
      * @see         String
      */
     public String pop() throws NoSuchElementException {
+		stack.push (s1);
     	//throw new NoSuchElementException();
     	String result = v.lastElement();
     	v.remove(result);
